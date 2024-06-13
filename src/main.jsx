3fc6600719d5c80a36d2210b671cd945e11cf65f
Augustine Khumalo <augustine@digitalsolutionfoundry.co.za>
2024-06-13 20:21:24 +0200

    fixing the corse issue and making sure that the backend is successfully connected to this frontend

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { ApolloProvider } from '@apollo/client';
 import client from './apollo-client';
 import App from './App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>
