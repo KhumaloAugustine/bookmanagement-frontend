b46e5d3f7614c10e1ab025824460be42a50d5291
Augustine Khumalo <augustine@digitalsolutionfoundry.co.za>
2024-06-16 15:35:36 +0200

    adjusted home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css'; 
 
 const Home = () => {
   return (
-    <div>
-      <h2>Welcome to Book Management</h2>
-    </div>
+    <section className="hero">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">
+            <h1>Welcome to Book Management</h1>
+            <p>Manage your books with ease and efficiency. Explore our collection of books and authors.</p>
+            <Link to="/books" className="btn btn-primary">Explore Books</Link>
+          </div>
+          <div className="col-lg-6">
+            {/* Optional: Add an image related to books */}
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnDjD5KiTN7kvesAbFvHcXWkNt2RTCC_IIxiyB3TsF0hYyXL5n1jAJBOBsHg&s" alt="Books" className="img-fluid" />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
